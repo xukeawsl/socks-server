5148fa1fcdd2a28e24d06ee6234d1249925c7278
xukeawsl <xukeawsl@gmail.com>
2022-10-29 11:02:14 +0800

    update ServerParser class

--- include/option/parser.h
+++ include/option/parser.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #include "common/common.h"
+#include "socks5/socks5_type.h"
 
 class ServerParser {
 public:
-    explicit ServerParser(const std::string& config_file);
-    ~ServerParser() = default;
+    static ServerParser* global_config() {
+        static ServerParser parser;
+        return &parser;
+    }
+
+    bool parse_config_file(const std::string& config_file);
 
     inline std::string get_host() const { return host; }
 
@@ -21,6 +26,22 @@ public:
         return max_rotate_count;
     }
 
+    inline bool is_supported_method(SocksV5::Method method) const {
+        return supported_methods.count(method) > 0;
+    }
+
+    inline bool check_username(const std::string& uname) const {
+        return username == uname;
+    }
+
+    inline bool check_password(const std::string& passwd) const {
+        return password == passwd;
+    }
+
+private:
+    explicit ServerParser();
+    ~ServerParser() = default;
+
 private:
     std::string host;
     uint16_t port;
@@ -28,4 +49,7 @@ private:
     std::string log_file;
     long unsigned max_rotate_size;
     long unsigned max_rotate_count;
+    std::string username;
+    std::string password;
+    std::unordered_set<SocksV5::Method> supported_methods;
 };
